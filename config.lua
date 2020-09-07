b7432e64ed3c9a92ae9d2095bfb586c2a3fdfee1
Jochem Pouwels <pouwelsjochem@gmail.com>
2020-09-07 11:56:49 +0200

    Removed 'scale' & imageSuffix attribute from config.lua

--- config.lua
+++ config.lua
@@ -5,7 +5,5 @@ application =
 				width = 1100,
 				height = 700,
 				fps = 30,
-				-- should be adaptive but that doesn't seem to work on Windows
-				scale = "letterbox", -- "adaptive",
 		}
 }
